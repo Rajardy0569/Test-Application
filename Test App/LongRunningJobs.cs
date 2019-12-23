aa6764b585322f1d1146892a0200f3df4c022bab
Rajardy0569 <Rajardy0569@users.noreply.github.com>
2019-12-23 19:44:23 +0530

    Initial commit

---
+++ Test App/LongRunningJobs.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Testing_Application
+{
+    public class LongRunningJobs
+    {
+        public void CopyDataToAnotherFile(Queue qe)
+        {
+            string clearDestinationFileData = @"C:\Test App\destination\file2.txt";
+            File.WriteAllText(clearDestinationFileData, String.Empty);
+            Log.Information("Copy Started    {Now}", DateTime.Now);
+            using (var reader = new System.IO.StreamReader(@"C:\Test App\src\file1.txt"))
+            using (var writer = new System.IO.StreamWriter(clearDestinationFileData))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    string line = reader.ReadLine();
+                    char[] stripped = line.Where(c => char.IsLetter(c)).ToArray();
+                    writer.WriteLine(stripped);
+                }
+            }
+            Log.Information("Copy Completed    {Now} ---{ThreadID}", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+            lock (qe)
+            {
+                Log.Information("(Dequeue)\t{0}", qe.Dequeue());
+            }
+            Console.WriteLine("Copy Job Completed");
+            Thread.Sleep(4000);
+        }
+
+        public void RandomNumbers(int frm, int to, Queue qe)
+        {
+            string filePath = @"C:\Test App\destination\RandomNumbers.txt";
+            File.WriteAllText(filePath, String.Empty);
+            Log.Information("RandomNumbers Printing Started  {Now}", DateTime.Now);
+            Random r = new Random();
+            int number = 0;
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.Write(DateTime.Now + "\n");
+                for (int i = 1; i < 10; i++)
+                {
+                    number = r.Next(frm, to);
+                    writer.Write(number + "\n");
+                }
+                writer.Write(DateTime.Now + "\n");
+            }
+            Log.Information("RandomNumbers Printing Completed  {Now} ---{Threadid}", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+            lock (qe)
+            {
+                Log.Information("(Dequeue)\t{0}", qe.Dequeue());
+            }
+            Console.WriteLine("RandomNumber Job Completed");
+            Thread.Sleep(4000);
+        }
+
+
+        public void Add(int a, int b, Queue qe)
+        {
+            Log.Information("Add job is started  {Now}", DateTime.Now);
+            Log.Information("Add result  {Sum}", a + b);
+            Log.Information("Add job is completed  {Now}----{Threadid}", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+            lock (qe)
+            {
+                Log.Information("(Dequeue)\t{0}", qe.Dequeue());
+            }
+            Thread.Sleep(7000);
+            Console.WriteLine("Addition Job Completed");
+        }
+        public void Subtract(int a, int b, Queue qe)
+        {
+            Log.Information("subtract job is started  {Now}", DateTime.Now);
+            Log.Information("subtract result  {Sum}", a - b);
+            Log.Information("subtract job is completed  {Now}------{Threadid}", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+            lock (qe)
+            {
+                Log.Information("(Dequeue)\t{0}", qe.Dequeue());
+            }
+            Console.WriteLine("Subtract Job Completed");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Test App/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using System.Collections;
+using Testing_Application;
+using System.Threading;
+
+namespace Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.File(@"C:\Logs\myapp.txt")
+               .CreateLogger();
+            Log.Information("Application starting");
+            Queue qe = new Queue();
+            Console.WriteLine("The below are the list of jobs \n 1. CopyDataToAnotherFile \n 2.RandomNumbers \n 3.Add \n 4.Subtract \n");
+            Console.WriteLine("Currently  1,2 number jobs are added to the queue, if you want you can add with respective number");
+            lock (qe)
+            {
+                qe.Enqueue(1);
+                qe.Enqueue(2);
+            }
+            // qe.Enqueue(3);
+            // qe.Enqueue(4);
+            // Thread t = new Thread(()=>MyMethodAsync(qe));
+            // Task task = Task.Run((Action)MyMethodAsync(qe));
+
+
+            string str = "";
+            while (str != null)
+            {
+                Thread t = new Thread(() => MyMethodAsync(qe));
+                t.IsBackground = true;
+                t.Start();
+                str = Console.ReadLine();
+                Console.WriteLine("Received input is " + str);
+                Log.Information("added to queue {Value}", str);
+                lock (qe)
+                {
+                    qe.Enqueue(Convert.ToInt32(str));
+                }
+            }
+            Console.ReadKey();
+            Log.CloseAndFlush();
+        }
+        static async Task MyMethodAsync(Queue qe)
+        {
+            LongRunningJobs longRunningJobs = new LongRunningJobs();
+            var t = Task.Factory.StartNew(delegate { });
+            int a = qe.Count;
+            for (int i = qe.Count; i >= qe.Count; i--)
+            {
+                string s = string.Empty;
+                lock (qe)
+                {
+                    s = qe.Peek().ToString();
+                }
+                
+                switch (s)
+                {
+
+                    case "1":
+                        // Log.Information("The number of elements in the Queue {Count}", qe.Count);
+                        await t.ContinueWith((prevTask) => longRunningJobs.CopyDataToAnotherFile(qe));
+                        break;
+
+                    case "2":
+                        // Log.Information("The number of elements in the Queue {Count}", qe.Count);
+                        await t.ContinueWith((prevTask) => longRunningJobs.RandomNumbers(1, 10, qe));
+                        break;
+
+                    case "3":
+                        // Log.Information("The number of elements in the Queue {Count}", qe.Count);
+                        await t.ContinueWith((prevTask) => longRunningJobs.Add(10, 100, qe));
+                        break;
+
+                    case "4":
+                        // Log.Information("The number of elements in the Queue {Count}", qe.Count);
+                        await t.ContinueWith((prevTask) => longRunningJobs.Subtract(100, 10, qe));
+                        break;
+                }
+            }
+        }
+    }
+}
